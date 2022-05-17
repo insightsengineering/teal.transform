9469fcc8f6903ff0da9f0396ae40b16158307a3e
kpagacz <konrad.pagacz@gmail.com>
2022-05-17 15:21:43 +0200

    added shinytest tests

---
+++ tests/testthat/shinytest/format_data_extract/app.R
@@ -0,0 +1,23 @@
+simple_des <- teal.transform::data_extract_spec(
+  dataname = "iris",
+  filter = teal.transform::filter_spec(vars = "Petal.Length", choices = c("1.4", "1.5")),
+  select = teal.transform::select_spec(choices = c("Petal.Length", "Species"))
+)
+
+sample_filtered_data <- {
+  data <- teal.data::teal_data(teal.data::dataset("iris", iris))
+  datasets <- teal.slice:::filtered_data_new(data)
+  teal.slice:::filtered_data_set(data, datasets)
+  datasets
+}
+
+ui <- shiny::fluidPage(
+  teal.transform::data_extract_ui(id = "des", label = "test des ui", data_extract_spec = simple_des),
+  shiny::verbatimTextOutput(outputId = "formatted_des"),
+)
+srv <- function(input, output, session) {
+    extracted_des <- teal.transform::data_extract_srv(id = "des", datasets = sample_filtered_data, data_extract_spec = simple_des)
+    output$formatted_des <- shiny::renderPrint(cat(teal.transform::format_data_extract(extracted_des())))
+}
+
+shiny::shinyApp(ui, srv)
