a495e5ddacb0b6ff162f37e0a54a885dd75723b8
Andrew Bates <andrew.bates@atorusresearch.com>
2022-12-13 11:39:09 -0800

    add checkmate assertions to *_validation_rule

--- R/data_extract_module.R
+++ R/data_extract_module.R
@@ -434,6 +434,9 @@ data_extract_srv.list <- function(id, datasets, data_extract_spec, join_keys = N
                                   ...) {
   checkmate::assert_list(datasets, types = c("reactive", "data.frame"), names = "named")
   checkmate::assert_class(join_keys, "JoinKeys", null.ok = TRUE)
+  checkmate::assert_function(select_validation_rule, null.ok = TRUE)
+  checkmate::assert_function(filter_validation_rule, null.ok = TRUE)
+  checkmate::assert_function(dataset_validation_rule, null.ok = TRUE)
 
   moduleServer(
     id,
@@ -681,6 +684,21 @@ data_extract_multiple_srv.list <- function(data_extract, datasets, join_keys = N
                                            }, ...) {
   checkmate::assert_list(datasets, types = c("reactive", "data.frame"), names = "named")
   checkmate::assert_class(join_keys, "JoinKeys", null.ok = TRUE)
+  checkmate::assert(
+    checkmate::check_function(select_validation_rule, null.ok = TRUE),
+    checkmate::check_list(select_validation_rule, types = "function", null.ok = TRUE),
+    checkmate::check_subset(names(select_validation_rule), names(data_extract))
+  )
+  checkmate::assert(
+    checkmate::check_function(filter_validation_rule, null.ok = TRUE),
+    checkmate::check_list(filter_validation_rule, types = "function", null.ok = TRUE),
+    checkmate::check_subset(names(filter_validation_rule), names(data_extract))
+  )
+  checkmate::assert(
+    checkmate::check_function(dataset_validation_rule, null.ok = TRUE),
+    checkmate::check_list(dataset_validation_rule, types = "function", null.ok = TRUE),
+    checkmate::check_subset(names(dataset_validation_rule), names(data_extract))
+  )
 
   logger::log_trace(
     "data_extract_multiple_srv.list initialized with dataset: { paste(names(datasets), collapse = ', ') }."
