2898b69cf036c0e002ec1297de78fa62a4a20406
Dawid Kałędkowski <6959016+gogonzo@users.noreply.github.com>
2023-12-05 15:53:27 +0100

    independent resolve_delay - no need teal (#165)

--- R/data_extract_single_module.R
+++ R/data_extract_single_module.R
@@ -73,11 +73,22 @@ data_extract_single_srv <- function(id, datasets, single_data_extract_spec) {
   moduleServer(
     id,
     function(input, output, session) {
-      logger::log_trace(
-        "data_extract_single_srv initialized with dataset: { single_data_extract_spec$dataname }."
-      )
-      for (idx in seq_along(single_data_extract_spec$filter)) {
-        x <- single_data_extract_spec$filter[[idx]]
+      logger::log_trace("data_extract_single_srv initialized with dataset: { single_data_extract_spec$dataname }.")
+
+      # ui could be initialized with a delayed select spec so the choices and selected are NULL
+      # here delayed are resolved
+      isolate({
+        resolved <- resolve_delayed(single_data_extract_spec, datasets)
+        teal.widgets::updateOptionalSelectInput(
+          session = session,
+          inputId = "select",
+          choices = resolved$select$choices,
+          selected = resolved$select$selected
+        )
+      })
+
+      for (idx in seq_along(resolved$filter)) {
+        x <- resolved$filter[[idx]]
         if (inherits(x, "filter_spec")) {
           data_extract_filter_srv(
             id = paste0("filter", idx),
