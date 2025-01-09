5993de173c79cc2c936f82188d880bd0aa7fc7b9
Aleksander Chlebowski <aleksander.chlebowski@sanofi.com>
2025-01-09 13:09:53 +0100

    fix typo

--- tests/testthat/test-delayed_choices.R
+++ tests/testthat/test-delayed_choices.R
@@ -4,7 +4,7 @@ testthat::test_that("delayed_choices constructors run without error", {
   testthat::expect_no_error(last_choice())
 })
 
-testthat::test_that("delayed_choices constructors return funcitons", {
+testthat::test_that("delayed_choices constructors return functions", {
   testthat::expect(is.function(all_choices()), "expecting function")
   testthat::expect(is.function(first_choice()), "expecting function")
   testthat::expect(is.function(last_choice()), "expecting function")
@@ -16,13 +16,13 @@ testthat::test_that("delayed_choices functions return appropriate subsets of ato
   testthat::expect_identical(last_choice()(letters), letters[length(letters)])
 })
 
-testthat::test_that("delayed_choices funcitons return passed NULL", {
+testthat::test_that("delayed_choices functions return passed NULL", {
   testthat::expect_null(all_choices()(NULL))
   testthat::expect_null(first_choice()(NULL))
   testthat::expect_null(last_choice()(NULL))
 })
 
-testthat::test_that("delayed_choices funcitons return passed empty vector", {
+testthat::test_that("delayed_choices functions return passed empty vector", {
   testthat::expect_identical(all_choices()(character(0L)), character(0L))
   testthat::expect_identical(first_choice()(character(0L)), character(0L))
   testthat::expect_identical(last_choice()(character(0L)), character(0L))
