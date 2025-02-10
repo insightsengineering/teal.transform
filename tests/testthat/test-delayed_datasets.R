b2b7e31fc2258deae9f8bbca0b2ec43e89b4e2f7
Aleksander Chlebowski <aleksander.chlebowski@sanofi.com>
2025-02-10 12:05:20 +0100

    add unit tests for delayed_datasets

---
+++ tests/testthat/test-delayed_datasets.R
@@ -0,0 +1,91 @@
+data <- teal.data::cdisc_data(
+  ADSL = teal.data::rADSL,
+  ADAE = teal.data::rADAE
+)
+
+des_current1 <- data_extract_spec(
+  dataname = "ADSL",
+  select = select_spec(
+    choices = variable_choices(
+      data = "ADSL",
+      subset = function(data) names(Filter(is.numeric, data))
+    ),
+    selected = first_choice()
+  )
+)
+
+des_current2 <- data_extract_spec(
+  dataname = "ADAE",
+  select = select_spec(
+    choices = variable_choices(
+      data = "ADAE",
+      subset = function(data) names(Filter(is.numeric, data))
+    ),
+    selected = first_choice()
+  )
+)
+
+des_delayed <- data_extract_spec(
+  dataname = delayed_datasets(),
+  select = select_spec(
+    choices = variable_choices(
+      data = delayed_datasets(),
+      subset = function(data) names(Filter(is.numeric, data))
+    ),
+    selected = first_choice()
+  )
+)
+
+des_resolved <- list(
+  des_current1,
+  des_current2
+)
+
+
+
+testthat::test_that("single current ddes is unchanged", {
+  testthat::expect_identical(
+    des_current1 |> resolve_delayed_datasets(names(data)),
+    des_current1
+  )
+})
+
+testthat::test_that("single delayed ddes is resolved into list of length(names(data))", {
+  testthat::expect_equal(
+    des_delayed |> resolve_delayed_datasets(names(data)),
+    des_resolved,
+    check.environment = FALSE
+  )
+})
+
+testthat::test_that("resolved des replaces parent level in nested list of length 1", {
+  # this reproduces what happens in data_extract_multiple_srv.list
+  testthat::expect_equal(
+    list(des_current1, des_delayed) |> resolve_delayed_datasets(names(data)),
+    list(des_current1, des_resolved),
+    check.environment = FALSE
+  )
+  testthat::expect_equal(
+    list(list(des_current1), list(des_delayed)) |> resolve_delayed_datasets(names(data)),
+    list(list(des_current1), des_resolved),
+    check.environment = FALSE
+  )
+})
+
+testthat::test_that("ddes with specified datasets resolves to intersection of those and the available ones", {
+  des_delayed_subset <- data_extract_spec(
+    dataname = delayed_datasets(c("ADSL", "ADEX")),
+    select = select_spec(
+      choices = variable_choices(
+        data = delayed_datasets(c("ADSL", "ADEX")),
+        subset = function(data) names(Filter(is.numeric, data))
+      ),
+      selected = first_choice()
+    )
+  )
+  testthat::expect_equal(
+    des_delayed_subset |> resolve_delayed_datasets(names(data)),
+    list(des_current1),
+    check.environment = FALSE
+  )
+})
