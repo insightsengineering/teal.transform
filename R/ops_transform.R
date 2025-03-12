6600d179d896a8d880d504db0bfc1dcc74f9d41d
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-03-12 15:29:25 +0100

    Use Group Generic Functions

--- R/ops_transform.R
+++ R/ops_transform.R
@@ -1,47 +1,33 @@
 #' @export
-`&.transform` <- function(e1, e2) {
-  if (!is.transform(e1) || !is.transform(e2)) {
-    stop("Method not available")
+Ops.transform <- function(e1, e2) {
+  if (missing(e2)) {
+    # out <- switch(.Generic,
+           # "!" = Negate,
+    stop("Method ", sQuote(.Generic), " not implemented for this class ", .Class, ".", call. = FALSE)
+    # return(out)
   }
-  o <- transform()
-  if (has_dataset(e1) || has_dataset(e2)) {
-    o$datasets <- c(e1$datasets, e2$datasets)
-    o$datasets <- o$datasets[!is.na(o$datasets)]
-  }
-  if (has_variable(e1) || has_variable(e2)) {
-    o$variables <- c(e1$variables, e2$variables)
-    o$variables <- o$variables[!is.na(o$variables)]
-  }
-  if (has_value(e1) || has_value(e2)) {
-    o$values <- c(e1$values, e2$values)
-    o$values <- o$values[!is.na(o$values)]
-  }
-
-  class(o) <- c("delayed", "transform")
-  o
+  switch(.Generic,
+         "!=" = NextMethod(),
+         # "==" = NextMethod(),
+         # "|" = ,
+         "&" = c(e1, e2),
+         stop("Method ", sQuote(.Generic), " not implemented for this class ", .Class, ".", call. = FALSE))
 }
 
 #' @export
-`|.dataset` <- function(e1, e2) {
-  if (!is.transform(e1) || !is.transform(e2)) {
-    stop("Method not available")
+Ops.type <- function(e1, e2) {
+  if (missing(e2)) {
+    # out <- switch(.Generic,
+    #        "!" = Negate,
+    stop("Method ", sQuote(.Generic), " not implemented for this class ", .Class, ".", call. = FALSE)
+    # return(out)
   }
-  s <- transform()
-  class(x) <- c("delayed", "transform")
-  x
+  out <- switch(.Generic,
+         "!=" = NextMethod(),
+         # "==" = NextMethod(),
+         # "|" = ,
+         "&" = c(e1, e2),
+         stop("Method ", sQuote(.Generic), " not implemented for this class ", .Class, ".", call. = FALSE))
+  class(out) <- class(e1)
+  out
 }
-
-# #' @export
-# chooseOpsMethod.transform <- function(x, y, mx, my, cl, reverse) {
-#   # cat("\nx\n")
-#   # print(mx)
-#   # cat("\ny\n")
-#   # print(my)
-#   # cat("\ncl\n")
-#   # print(cl)
-#   # cat("\nreverse\n")
-#   # print(reverse)
-#   is.transform(x)
-# }
-
-# ?Ops
