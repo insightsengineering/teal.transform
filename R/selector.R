10509da325a39eca8ee0481480f5d13c2225270b
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-05-09 13:22:35 +0200

    Match expectations based on names

--- R/selector.R
+++ R/selector.R
@@ -1,6 +1,7 @@
 selector <- function(data, ...) {
   if (is.environment(data)) {
-    data <- as.list(data)
+    # To keep the "order" of the names in the extraction: avoids suprises
+    data <- as.list(data)[names(data)]
   } else if (length(dim(data)) == 2L) {
     data <- as.data.frame(data)
   }
