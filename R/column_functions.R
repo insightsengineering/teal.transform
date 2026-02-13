473fa758d0681e59454c1558548c3513b5f80ed9
osenan <oriol.senan@businesspartner.roche.com>
2026-02-13 11:55:14 +0100

    chore: fix lintr

--- R/column_functions.R
+++ R/column_functions.R
@@ -12,7 +12,8 @@
 #' @export
 #'
 get_dataset_prefixed_col_names <- function(data) {
-  if (!is.null(attr(data, "filter_and_columns", exact = TRUE)$columns) && all(attr(data, "filter_and_columns", exact = TRUE)$columns != "")) {
+  if (!is.null(attr(data, "filter_and_columns", exact = TRUE)$columns) &&
+        all(attr(data, "filter_and_columns", exact = TRUE)$columns != "")) {
     paste(attr(data, "dataname"), attr(data, "filter_and_columns")$columns, sep = ".")
   } else {
     NULL
